2ccd15219f233ff64fa56760b434958aa2425ec1
pampers <ivantodoroshko@gmail.com>
2026-03-05 22:31:34 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,7 +4,12 @@
 
 int main(){
 	initscr();
+	noecho();
 
-
+	WINDOW* BORDER = newwin(ROWS + 2,(COLS*2) +2, 0, 0);
+	refresh();
+	box(BORDER, 0, 0);
+	wrefresh(BORDER);
+	int c = getch();
 	endwin();
 }
